2ffe68f4e55424a120e2efd8aadb0e2f5ca95aec
iamphduc <iamphduc@gmail.com>
2022-09-09 14:23:21 +0700

    refactor: lazy loading image

--- scripts/lax.js
+++ scripts/lax.js
@@ -107,6 +107,32 @@ window.onload = () => {
 
   // ===== TIMELINE ===== //
 
+  const timelineImages = document.querySelectorAll(".timeline img");
+
+  const timelineImagesOptions = {
+    rootMargin: "-200px 0px 0px 0px",
+  };
+
+  timelineImages.forEach((image, i) => {
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const lazyImage = entry.target;
+          const src = lazyImage.dataset.src;
+
+          if (lazyImage.tagName.toLowerCase() === "img") {
+            lazyImage.src = src;
+          }
+
+          lazyImage.removeAttribute("data-src");
+          observer.unobserve(lazyImage);
+        }
+      });
+    }, timelineImagesOptions);
+
+    imageObserver.observe(image);
+  });
+
   lax.addElements(".timeline__img-container", {
     scrollY: {
       translateX: [
